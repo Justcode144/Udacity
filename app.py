301e51e00bba214730f7ba661c42cb99fb563304
Justcode144 <Justcode144@gmail.com>
2022-03-16 22:20:18 -0400

    Crud Application with SQL database

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234007@localhost:5432/TODO1'
+db = SQLAlchemy (app)
+
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id= db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f' <Todo {self.id} {self.descripiton}>'
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  description = request.form.get('description', '')
+  todo = Todo(description=description)
+  db.session.add(todo)
+  db.session.commit()
+  return redirect(url_for('create_todo'))
+
+@app.route('/')
+def index():
+  return render_template('todoapp.html', data=Todo.query.all()
+                         )
+
+db.create_all()
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=3000)
+
+print(__name__)
